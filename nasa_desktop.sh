5cb4133944daf8c4d9d2239b409cc533e4c6ace4
rocktronica <tommy@iamnotagoodartist.com>
2015-04-24 07:22:59 -0700

    Fix URL to be absolute for CURL

--- nasa_desktop.sh
+++ nasa_desktop.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
 {
+    path='http://apod.nasa.gov/apod'
     date_slug=$(date "+%y%m%d")
     image_filename="$PWD/images/$date_slug.jpg"
     cache_page_filename="$PWD/cache/ap$date_slug.html"
 
     function download_page() {
         if [ ! -f $cache_page_filename ]; then
-            curl -# -L http://apod.nasa.gov/apod/ap$date_slug.html \
+            curl -# -L $path/ap$date_slug.html \
                 > $cache_page_filename
         fi
     }
 
-    function get_url() {
-        cat $cache_page_filename |
-            grep -oE "href=[^>]*" | \
-            grep -oE "[^'\"]*.jpg"
+    function get_absolute_jpg_url() {
+        echo $path/$(
+            cat $cache_page_filename |
+                grep -oE "href=[^>]*" | \
+                grep -oE "[^'\"]*.jpg"
+        )
     }
 
     function download_image() {
         if [ ! -f $image_filename ]; then
-            image_url=$(get_url)
+            image_url=$(get_absolute_jpg_url)
             echo "$image_url\n"
             curl -# $image_url > $image_filename
         fi
